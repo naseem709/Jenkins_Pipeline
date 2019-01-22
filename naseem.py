b0fc459267172732c42f1922cd77104326e8adf2
naseem709 <naseemahmad262902@gmail.com>
2019-01-22 20:11:36 +0530

    Python module for jenkins

--- naseem.py
+++ naseem.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
  
-numbers = (1, 2, 3, 4, 5, 6, 7, 8, 9) # Declaring the tuple
+numbers = (1, 2, 3, 4, 5, 6, 7, 8, 9) # Declaring the tuple.
 count_odd = 0
 count_even = 0
 for x in numbers:
